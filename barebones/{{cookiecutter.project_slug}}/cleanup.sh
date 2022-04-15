20a10a8d0869e2e5f2ccb04557842ed47108adc9
chikaj <ncurrit@gmail.com>
2022-04-15 13:33:15 -0500

    Fixed volume on docker-compose and cleanup.sh

--- barebones/{{cookiecutter.project_slug}}/cleanup.sh
+++ barebones/{{cookiecutter.project_slug}}/cleanup.sh
@@ -2,3 +2,4 @@ docker rm {{cookiecutter.project_slug}}_postgis
 docker rmi {{cookiecutter.project_slug}}_postgis
 docker network rm {{cookiecutter.project_slug}}_barebones_network
 docker volume rm {{cookiecutter.project_slug}}_postgis_data
+docker volume rm {{cookiecutter.project_slug}}_pgadmin4_data
