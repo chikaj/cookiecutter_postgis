0cac968f991adce9e4990786b05e839c83b0b117
chikaj <ncurrit@gmail.com>
2022-08-16 16:16:20 -0500

    Fixed cleanup.sh

--- postgrest/{{cookiecutter.project_slug}}/cleanup.sh
+++ postgrest/{{cookiecutter.project_slug}}/cleanup.sh
@@ -4,8 +4,6 @@ docker rm {{cookiecutter.project_slug}}_postgrest
 docker rm {{cookiecutter.project_slug}}_swagger
 docker rmi {{cookiecutter.project_slug}}_postgis
 docker rmi {{cookiecutter.project_slug}}_pgadmin4
-docker rmi {{cookiecutter.project_slug}}_postgrest
-docker rmi {{cookiecutter.project_slug}}_swagger
 docker network rm {{cookiecutter.project_slug}}_postgrest_network
 docker volume rm {{cookiecutter.project_slug}}_postgis_data
 docker volume rm {{cookiecutter.project_slug}}_pgadmin4_data
