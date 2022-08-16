336b8bb1c68cc18e833ed108eae56966bcd6e692
chikaj <ncurrit@gmail.com>
2022-08-16 15:40:03 -0500

    Fixed postgrest and swagger .env files, cleanup.sh, docker-compose

--- postgrest/{{cookiecutter.project_slug}}/cleanup.sh
+++ postgrest/{{cookiecutter.project_slug}}/cleanup.sh
@@ -1,7 +1,11 @@
 docker rm {{cookiecutter.project_slug}}_postgis
 docker rm {{cookiecutter.project_slug}}_pgadmin4
+docker rm {{cookiecutter.project_slug}}_postgrest
+docker rm {{cookiecutter.project_slug}}_swagger
 docker rmi {{cookiecutter.project_slug}}_postgis
 docker rmi {{cookiecutter.project_slug}}_pgadmin4
-docker network rm {{cookiecutter.project_slug}}_barebones_network
+docker rmi {{cookiecutter.project_slug}}_postgrest
+docker rmi {{cookiecutter.project_slug}}_swagger
+docker network rm {{cookiecutter.project_slug}}_postgrest_network
 docker volume rm {{cookiecutter.project_slug}}_postgis_data
 docker volume rm {{cookiecutter.project_slug}}_pgadmin4_data
