86c8a522a45c5808d1e772a4fd36762e9ff1ebce
chikaj <ncurrit@gmail.com>
2022-11-10 16:31:38 -0600

    trying to fix pg_fetureserv

--- mapping/{{cookiecutter.project_name}}/cleanup.sh
+++ mapping/{{cookiecutter.project_name}}/cleanup.sh
@@ -1,7 +1,11 @@
 docker rm {{cookiecutter.project_slug}}_postgis
 docker rm {{cookiecutter.project_slug}}_pgadmin4
+docker rm {{cookiecutter.project_slug}}_pg_tileserv
+docker rm {{cookiecutter.project_slug}}_pg_featureserv
 docker rmi {{cookiecutter.project_slug}}_postgis
 docker rmi {{cookiecutter.project_slug}}_pgadmin4
+docker rmi {{cookiecutter.project_slug}}_pg_tileserv
+docker rmi {{cookiecutter.project_slug}}_pg_featureserv
 docker network rm {{cookiecutter.project_slug}}_barebones_network
 docker volume rm {{cookiecutter.project_slug}}_postgis_data
 docker volume rm {{cookiecutter.project_slug}}_pgadmin4_data
