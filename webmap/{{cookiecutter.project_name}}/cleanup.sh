953843a34e732475c532279e7df93728da8923ed
chikaj <ncurrit@gmail.com>
2022-11-14 14:17:34 -0600

    fix docker-compose volume and cleanup.sh

--- webmap/{{cookiecutter.project_name}}/cleanup.sh
+++ webmap/{{cookiecutter.project_name}}/cleanup.sh
@@ -6,6 +6,6 @@ docker rmi {{cookiecutter.project_slug}}_postgis
 docker rmi {{cookiecutter.project_slug}}_pgadmin4
 docker rmi {{cookiecutter.project_slug}}_pg_tileserv
 docker rmi {{cookiecutter.project_slug}}_pg_featureserv
-docker network rm {{cookiecutter.project_slug}}_barebones_network
+docker network rm {{cookiecutter.project_slug}}_mapping_network
 docker volume rm {{cookiecutter.project_slug}}_postgis_data
 docker volume rm {{cookiecutter.project_slug}}_pgadmin4_data
