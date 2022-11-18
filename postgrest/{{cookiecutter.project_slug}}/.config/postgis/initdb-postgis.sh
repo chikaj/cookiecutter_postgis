2bfbbb9fab7dd83f7cfd009a67f64f2d6ca42709
chikaj <ncurrit@gmail.com>
2022-11-18 17:16:46 -0600

    prepping for anonymous access via pg_featureserv and pg_tileserv

--- postgrest/{{cookiecutter.project_slug}}/.config/postgis/initdb-postgis.sh
+++ postgrest/{{cookiecutter.project_slug}}/.config/postgis/initdb-postgis.sh
@@ -13,13 +13,13 @@ for DB in "{{cookiecutter.postgres_primary_db}}"; do
 	psql --dbname={{cookiecutter.postgres_primary_db}} --username={{cookiecutter.postgres_primary_user}} <<-'EOSQL'
 		-- CREATE SCHEMA AUTHORIZATION {{cookiecutter.postgres_primary_user}};
 		CREATE SCHEMA postgis AUTHORIZATION {{cookiecutter.postgres_primary_user}};
-		CREATE SCHEMA topology AUTHORIZATION {{cookiecutter.postgres_primary_user}};
+		-- CREATE SCHEMA topology AUTHORIZATION {{cookiecutter.postgres_primary_user}};
 	EOSQL
 	echo "Creating PostGIS extensions in {{cookiecutter.postgres_primary_db}}"
 	psql --dbname={{cookiecutter.postgres_primary_db}} --username={{cookiecutter.postgres_primary_user}} <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis SCHEMA postgis;
 		CREATE EXTENSION IF NOT EXISTS postgis_raster SCHEMA postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology SCHEMA topology;
+		-- CREATE EXTENSION IF NOT EXISTS postgis_topology SCHEMA topology;
 		ALTER DATABASE {{cookiecutter.postgres_primary_db}} SET postgis.enable_outdb_rasters = true;
 		ALTER DATABASE {{cookiecutter.postgres_primary_db}} SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
 		-- ALTER ROLE $PGUSER SET search_path=public,postgis;
